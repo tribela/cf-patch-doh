f48c9bdd784e3297b1cb9cd5b8f6000ec7bfeda5
Jeong Arm <kjwonmail@gmail.com>
2025-04-16 20:21:16 +0900

    Fix expire on TtlCache

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import base64
 import os
+import time
 
 import dnslib
 import httpx
+
+from dns_utils import TtlCache
 
 domains = [
     'qdon.space',
@@ -75,9 +78,41 @@ def test_upstream_path(domain):
     print(res.elapsed.total_seconds())
 
 
+def test_cache():
+    cache: TtlCache[str, str] = TtlCache(max_size=3, max_ttl=2)
+
+    print('Testing expire')
+    cache.store('a', 'b', 1)
+    assert cache.get('a') == 'b'
+    time.sleep(1)
+    assert cache.get('a') is None
+
+    print('Testing max_ttl')
+    cache.store('a', 'b', 4)
+    assert cache.get('a') == 'b'
+    time.sleep(3)
+    assert cache.get('a') is None
+
+    print('Testing default ttl')
+    cache.store('a', 'b')
+    assert cache.get('a') == 'b'
+    time.sleep(2)
+    assert cache.get('a') is None
+
+    print('Testing max_size')
+    for i in range(10):
+        cache.store(str(i), str(i))
+
+    values = [cache.get(str(i)) for i in range(10)]
+    values = [v for v in values if v is not None]
+    assert len(values) == 3
+
+
 for domain in domains * 3:
     print(domain)
     test_post(domain)
     test_get(domain)
     test_upstream_path(domain)
     print()
+
+test_cache()
